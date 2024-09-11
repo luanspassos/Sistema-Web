f980044d20cd3e2130654cdbf32913d37dfe3160
Luan Passos <lpassos@americastg.com>
2024-09-11 18:36:44 -0300

    SellersController

---
+++ Sales_Web_MVC/Controllers/SellersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sales_Web_MVC.Controllers
+{
+    public class SellersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
