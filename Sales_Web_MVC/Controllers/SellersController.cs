5f0069f31c45b4539a0f17efe6773497a7d01ec5
Luan Passos <lpassos@americastg.com>
2025-01-13 17:32:27 -0300

    Exception handling for delete (referencial integrity)

--- Sales_Web_MVC/Controllers/SellersController.cs
+++ Sales_Web_MVC/Controllers/SellersController.cs
@@ -56,8 +56,15 @@ namespace Sales_Web_MVC.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Delete(int id)
         {
-            await _sellerService.RemoveAsync(id);
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                await _sellerService.RemoveAsync(id);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (IntegrityException e)
+            {
+                return RedirectToAction(nameof(Error), new { message = e.Message });
+            }
         }
 
 
--- Sales_Web_MVC/Services/SellerService.cs
+++ Sales_Web_MVC/Services/SellerService.cs
@@ -32,10 +32,17 @@ namespace Sales_Web_MVC.Services
 
         public async Task RemoveAsync(int id)
         {
-            var seller = await _context.Seller.FindAsync(id);  //Seller seller = await _context.Seller.FirstOrDefaultAsync(s => s.Id == id);
+            try
+            {
+                var seller = await _context.Seller.FindAsync(id);  //Seller seller = await _context.Seller.FirstOrDefaultAsync(s => s.Id == id);
 
-            _context.Remove(seller);
-            await _context.SaveChangesAsync();
+                _context.Remove(seller);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Can't delete seller because he/she has sales");
+            }
         }
 
         public async Task UpdateAsync(Seller seller)
